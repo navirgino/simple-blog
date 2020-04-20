fcd09ebd55d9406678f684ed0fb5b469805ee11f
Nico Virgino <nicovirgino@gmail.com>
2020-04-20 12:26:52 -0500

    progressing in the tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
     #define an action inside a controller
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
     end
+
+    # when a form is submitted, the field of the form are sent to rails as parameters.
+    # these parameters can then be referenced inside the controller actions, typically to perform 
+    # a particular task. to see what these parameters look like, change the create
+    # action to this:
+  
+
+    def create
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+
+    
+
 end
