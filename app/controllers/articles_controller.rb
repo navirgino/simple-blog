a359ebae39e83a301deedf790d79e95592b6785b
Nico Virgino <nicovirgino@gmail.com>
2020-04-20 20:32:49 -0500

    fixing error bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,30 +3,38 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
     def show
         @article = Article.find(params[:id])
     end
+
     def new
         @article = Article.new
     end
-
-    # when a form is submitted, the field of the form are sent to rails as parameters.
-    # these parameters can then be referenced inside the controller actions, typically to perform 
-    # a particular task. to see what these parameters look like, change the create
-    # action to this:
   
+    def edit
+        @article = Article.find(params[:id])
+    end
 
     def create
         @article = Article.new(article_params)
 
         if @article.save
-        redirect_to @article
+            redirect_to @article
         else
             render 'new'
         end
     end
 
-    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+  
     private
     def article_params
         params.require(:article).permit(:title, :text)
