9194627cdc1165e170c823ee275a68da67ea8a09
Nico Virgino <nicovirgino@gmail.com>
2020-04-21 14:50:31 -0500

    finished rails tutorial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,21 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+
+        redirect_to article_path(@article)
+    end
+    # or like this: 
+    #def destroy
+    #@article = Article.find(params[:article_id])
+    #@comment = @article.comments.find(params[:id])
+    #@comment.destroy
+    #redirect_to article_path(@article)
+  #end
+
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
